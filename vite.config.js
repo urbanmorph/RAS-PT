8d3cd8dcafb38d617e87e56b2841b36614b84442
Deep <deep@euclidxr.com>
2022-06-16 16:12:05 +0530

    Move code to src folder and test npm scripts

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  root: 'src',
+  base: '/RAS-PT',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: resolve(__dirname, 'src'),
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        "bus_stops": resolve(__dirname, 'src/bus_stops/index.html')
+      }
+    }
+  }
+})
