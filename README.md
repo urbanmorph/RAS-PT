9295425beaee64264cf45bef3c7d94c25a31a17f
Deep <deep@euclidxr.com>
2022-06-17 12:23:21 +0530

    Add prettier and husky precommit hook

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 # RAS-PT
-___
+
 This repo contains the source code for the RAS-PT initiative.  
 [Reliable, Accessible and Safe - Public Transport](https://urbanmorph.github.io/RAS-PT/)
-
 
 ## Getting started
 
 ### Prerequisites
+
 - Install [node.js](https://nodejs.org/en/download/)
 
 ### Local development
+
 - Navigate to the repo root folder
 - Run `npm install`
 - Run `npm start`
 
 This starts a local server with live-reload available at [http://localhost:3000](http://localhost:3000)
 
-
 ### Building for production
+
 - Run `npm run build`
 
 This creates a production ready build in the `dist` folder
 
-
 ### Preview production build
+
 - Run `npm run preview`
 
 Preview the production build on local
-
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  root: 'src',
-  base: '/RAS-PT',
+  root: "src",
+  base: "/RAS-PT",
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
-      external: resolve(__dirname, 'src'),
+      external: resolve(__dirname, "src"),
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        "bus_stops": resolve(__dirname, 'src/bus_stops/index.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "src/index.html"),
+        bus_stops: resolve(__dirname, "src/bus_stops/index.html"),
+      },
+    },
+  },
+});
