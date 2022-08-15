c17441a8f532c909f31be5cfbd2bf1c1d51e0cac
Deep <deep@euclidxr.com>
2022-08-15 20:18:28 +0530

    Folder restructure and code refactor

--- src/bus_stops_accessibility/stop_access_people.js
+++ src/bus_stops_accessibility/stop_access_people.js
@@ -1,166 +1,156 @@
-import busStops2018 from "./data/bus-stops-2018.json?url";
-import stopAccessibility2018 from "./data/stop-accessbility-2018.geojson?url";
+import { BASE_URL, pageLoader, perc2color } from "../utils";
 
-function replacer(key, value) {
-  if (key == "intersect_poly") return undefined;
-  else if (key == "access_poly") return undefined;
-  else if (key == "no_access_poly") return undefined;
-  else return value;
-}
+let intersection_polygons_2018,
+  non_intersection_polygons_2018,
+  markers = [],
+  popupLayer;
 
-function perc2color(perc) {
-  var r,
-    g,
-    b = 0;
-  g = 255;
-  r = Math.round(2.55 * (100 - perc));
-  var h = r * 0x10000 + g * 0x100 + b * 0x1;
-  return "#" + ("000000" + h.toString(16)).slice(-6);
-}
+class LeafletMap {
+  constructor(domId, center) {
+    this.mapInstance = null;
+    this.data = {
+      stops: [],
+      accessibility: [],
+    };
 
-var map = L.map("map").setView(
-  [12.965, 77.60],
-  11
-);
-var intersection_polygons_2018;
-var non_intersection_polygons_2018;
-var chart;
-var markers = [];
-var popupLayer;
-var busStops;
-
-// Add basemap
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  minZoom: 9,
-  attribution:
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetmap</a> contributors. <a href="https://www.openstreetmap.org/fixthemap">Edit the map</a>',
-  opacity: 0.5,
-  maxBoundsViscosity: 0.9,
-}).addTo(map);
-
-$.getJSON(busStops2018, function (busStopsData) {
-  // Add circle marker and bind tooltip with stop name for each entry of the bus stops data
-  busStops = busStopsData;
-});
-
-map.on('zoomend', function() {
-  if (map.getZoom() > 14) {
-    busStops.forEach(function (b) {
-      var circle = L.circle([b.lat, b.lon], {
-        bubblingMouseEvents: true,
-        color: "black",
-        dashArray: null,
-        dashOffset: null,
-        fill: true,
-        fillColor: "#000000",
-        radius: 1,
-        stroke: true,
-      }).addTo(map);
-      circle.bindTooltip(
-        `<div>
-                   ${b.stop_name}
-               </div>`,
-        { sticky: true }
-      );
-      markers.push(circle);
-    });
-  } else {
-    markers.forEach(function(l) {
-      map.removeLayer(l);
-    })
+    this.initMap(domId, center);
+    this.initEvents();
   }
-});
 
-$.getJSON(stopAccessibility2018, function (geojson) {
-  L.choropleth(geojson, {
-    valueProperty: "access_percentage",
-    scale: ["red", "green"],
-    steps: 4,
-    mode: "k",
-    style: {
-      color: "#fff",
-      weight: 2,
-      fillOpacity: 0.66,
-    },
-    onEachFeature: function (feature, layer) {
-      var pieChart =
-        "<h3>" + feature.properties.PS_Name + "</h3><div class='canvas-div'><canvas id=\"canvas\"></canvas></div>";
+  initMap = (domId, center) => {
+    this.mapInstance = L.map(domId).setView(center, 11);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      minZoom: 9,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetmap</a> contributors. <a href="https://www.openstreetmap.org/fixthemap">Edit the map</a>',
+      opacity: 0.5,
+      maxBoundsViscosity: 0.9,
+    }).addTo(this.mapInstance);
+  };
 
-      layer.bindPopup(pieChart).on("popupopen", () => {
-        popupLayer = layer;
+  initEvents = () => {
+    this.mapInstance.on("zoomend", () => {
+      if (this.mapInstance.getZoom() > 14) {
+        this.data.stops.forEach((b) => {
+          const circle = L.circle([b.lat, b.lon], {
+            bubblingMouseEvents: true,
+            color: "black",
+            fill: true,
+            fillColor: "#000000",
+            radius: 1,
+            stroke: true,
+          }).addTo(this.mapInstance);
+          circle.bindTooltip(
+            `<div>
+              ${b.stop_name}
+            </div>`,
+            { sticky: true }
+          );
+          markers.push(circle);
+        });
+      } else {
+        markers.forEach((l) => {
+          this.mapInstance.removeLayer(l);
+        });
+      }
+    });
+  };
 
-        try {
-          var stops = feature.properties.stops;
-          var labels = stops.map((stop) => stop.stop);
-          var data = stops.map((stop) => stop.percent_of_area);
-          var colors = stops.map((stop) => perc2color(stop.percent_of_area));
-        } catch {
-          console.log("No stops for selected booth.");
-          var labels = [];
-          var data = [];
-          var colors = [];
-        }
+  updateData = async (files) => {
+    const [stopsData, accessibilityData] = await pageLoader(files);
 
-        labels.push("No Stop");
-        data.push(feature.properties.no_access_percentage);
-        colors.push("rgba(255, 0, 0, 0.8)");
+    this.data.stops = stopsData;
 
-        var ctx = canvas.getContext("2d");
-        var config = {
-          type: "pie",
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                label: "Stops",
-                data: data,
-                backgroundColor: colors,
-              },
-            ],
-          },
-          options: {
-            maintainAspectRatio: false,
-            reponsive: true,
-          },
-        };
+    L.choropleth(accessibilityData, {
+      valueProperty: "access_percentage",
+      scale: ["red", "green"],
+      steps: 4,
+      mode: "k",
+      style: {
+        color: "#fff",
+        weight: 2,
+        fillOpacity: 0.66,
+      },
+      onEachFeature: (feature, layer) => {
+        const pieChart = `<h3>${feature.properties.PS_Name}</h3>
+          <div class='canvas-div'>
+            <canvas id="canvas"></canvas>
+          </div>
+        `;
 
-        chart = new Chart(ctx, config);
+        layer.bindPopup(pieChart).on("popupopen", () => {
+          popupLayer = layer;
+          const { stops = [] } = feature.properties;
+          const labels = [...stops.map((stop) => stop.stop), "No Stop"];
+          const data = [
+            ...stops.map((stop) => stop.percent_of_area),
+            feature.properties.no_access_percentage,
+          ];
+          const colors = [
+            ...stops.map((stop) => perc2color(stop.percent_of_area)),
+            "rgba(255, 0, 0, 0.8)",
+          ];
 
-        var element = document.getElementsByClassName("canvas-div")[0];
-        var height = "height:" + (300 + 25 * (stops.length - 5)) + "px;";
-        element.setAttribute("style", height);
-        var width = "width:" + (300 + 25 * (stops.length - 5)) + "px;";
-        element.setAttribute("style", width);
+          new Chart(canvas.getContext("2d"), {
+            type: "pie",
+            data: {
+              labels: labels,
+              datasets: [
+                {
+                  label: "Stops",
+                  data: data,
+                  backgroundColor: colors,
+                },
+              ],
+            },
+            options: {
+              maintainAspectRatio: false,
+              reponsive: true,
+            },
+          });
 
-        var element = document.getElementsByClassName(
-          "leaflet-popup-content"
-        )[0];
-        var height = "height:" + (300 + 25 * (stops.length - 5)) + "px;";
-        element.setAttribute("style", height);
-        var width = "width:" + (300 + 25 * (stops.length - 5)) + "px;";
-        element.setAttribute("style", width);
-      });
-      ////layer.bindTooltip('Properties: <br><pre>' + JSON.stringify(feature.properties, replacer, 4), {
+          const canvasElement =
+            document.getElementsByClassName("canvas-div")[0];
+          canvasElement.style.height = `${300 + 25 * (stops.length - 5)}px`;
+          canvasElement.style.width = `${300 + 25 * (stops.length - 5)}px`;
 
-      layer.on("mouseover", function () {
-        non_intersection_polygons_2018 = L.polygon(
-          feature.properties.no_access_poly.coordinates,
-          { color: "red", interactive: false }
-        ).addTo(map);
-        intersection_polygons_2018 = L.polygon(
-          feature.properties.access_poly.coordinates,
-          { color: "blue", interactive: false }
-        ).addTo(map);
-        if (popupLayer !== undefined) {
-          popupLayer.closePopup();
-        }
-      });
+          const element = document.getElementsByClassName(
+            "leaflet-popup-content"
+          )[0];
+          element.style.height = `${300 + 25 * (stops.length - 5)}px`;
+          element.style.width = `${300 + 25 * (stops.length - 5)}px`;
+        });
 
-      layer.on("mouseout", function () {
-        non_intersection_polygons_2018.remove();
-        intersection_polygons_2018.remove();
-      });
-    },
-  }).addTo(map);
-});
+        layer.on("mouseover", () => {
+          non_intersection_polygons_2018 = L.polygon(
+            feature.properties.no_access_poly.coordinates,
+            { color: "red", interactive: false }
+          ).addTo(this.mapInstance);
+          intersection_polygons_2018 = L.polygon(
+            feature.properties.access_poly.coordinates,
+            { color: "blue", interactive: false }
+          ).addTo(this.mapInstance);
+          if (popupLayer !== undefined) {
+            popupLayer.closePopup();
+          }
+        });
+
+        layer.on("mouseout", function () {
+          if (non_intersection_polygons_2018) {
+            non_intersection_polygons_2018.remove();
+          }
+          if (intersection_polygons_2018) {
+            intersection_polygons_2018.remove();
+          }
+        });
+      },
+    }).addTo(this.mapInstance);
+  };
+}
+
+const leafletInstance = new LeafletMap("map", [12.965, 77.6]);
+leafletInstance.updateData([
+  `${BASE_URL}bus-stops-2018.json`,
+  `${BASE_URL}stop-accessbility-2018.geojson`,
+]);
